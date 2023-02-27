d720fad94548af7cf667cb8b3e8ce89b0d247367
Glen-Roe <glen.roe.888@my.csun.edu>
2023-02-27 10:28:48 -0800

    first push

---
+++ src/compiler/Token.java
@@ -0,0 +1,26 @@
+package compiler;
+
+public class Token {
+
+	  private final String type;
+
+	  private final String value;
+
+	  public Token(String type) {
+	    this.type = type;
+	    this.value = null;
+	  }
+
+	  public Token(String type, String value) {
+	    this.type = type;
+	    this.value = value;
+	  }
+
+	  public String getType() {
+	    return type;
+	  }
+
+	  public String getValue() {
+	    return value;
+	  }
+	}
